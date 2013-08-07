f889c91efb23b49040bba75ff737e6f0d17d0bd3
Vamshi <vamshi.jandhyala@gmail.com>
2013-08-07 13:00:29 +0100

    Handy js scripts

---
+++ page_links.js
@@ -0,0 +1,21 @@
+var system = require('system');
+if (system.args.length > 1) {
+    url = Array.prototype.slice.call(system.args, 1);
+} else {
+    console.log("Usage: phantomjs page_links.js <pagelink>");
+}
+var page = require('webpage').create();
+page.settings.userName = "BlackRock";
+page.settings.password = "inV3st";
+page.settings.userAgent = "Googlebot";
+page.open(url, function () {
+	var links = page.evaluate(function() {
+        	return [].map.call(document.querySelectorAll('a'), function(link) {
+	            return link.getAttribute('href');
+        	});
+    	});
+
+console.log(links.join('\n'));
+phantom.exit();
+});
+
